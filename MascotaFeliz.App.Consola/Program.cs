ee63aa88cebcbcce09858eb7172c8921a3ab4206
lizzbejarano02 <lizzethbejarano620@gmail.com>
2022-09-06 21:48:23 -0500

    Primer commit

---
+++ MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+using System.Collections.Generic;
+
+namespace MascotaFeliz.App.Consola
+{
+    class Program
+    {  
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AddDueno();
+        }
+
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                //Cedula = "1212",
+                Nombres = "Juan",
+                Apellidos = "Sin Miedo",
+                Direccion = "Bajo un puente",
+                Telefono = "1234567891",
+                Correo = "juansinmiedo@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
+        }
+    }
+}
