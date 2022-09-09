02680fa82a58019d12bd2feafc0a7e0e4f70f80f
larry4043 <larry4043@gmail.com>
2022-09-08 21:48:56 -0400

    Cargando

--- MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App.Consola/Program.cs
@@ -6,10 +6,16 @@ using System.Collections.Generic;
 namespace MascotaFeliz.App.Consola
 {
     class Program
-    {  
+    {
         private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
 
         private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+
+        private static IRepositorioMascota _repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+
+        //private static IRepositorioHistoria _repoHistoria = new RepositorioHistoria(new Persistencia.AppContext());
+
+        //private static IRepositorioVisitasPyP _repoVisitasPyP = new RepositorioVisitasPyP(new Persistencia.AppContext());
 
         static void Main(string[] args)
         {
@@ -17,18 +23,19 @@ namespace MascotaFeliz.App.Consola
 
             AddDueno();
             AddVeterinario();
+            AddMascota();
         }
 
         private static void AddDueno()
         {
             var dueno = new Dueno
             {
-                //Cedula = "1212",
-                Nombres = "Juan",
-                Apellidos = "Sin Miedo",
-                Direccion = "Bajo un puente",
-                Telefono = "1234567891",
-                Correo = "juansinmiedo@gmail.com"
+                //Cedula = "1313",
+                Nombres = "El chavo",
+                Apellidos = "Del ocho",
+                Direccion = "El vecindario",
+                Telefono = "3345643210",
+                Correo = "elchavo@gmail.com"
             };
             _repoDueno.AddDueno(dueno);
         }
@@ -37,7 +44,7 @@ namespace MascotaFeliz.App.Consola
         {
             var veterinario = new Veterinario
             {
-                //Cedula = "1313",
+                
                 Nombres = "Lola",
                 Apellidos = "Mento",
                 Direccion = "Cali",
@@ -45,6 +52,22 @@ namespace MascotaFeliz.App.Consola
                 TarjetaProfesional = "1234567890"
             };
             _repoVeterinario.AddVeterinario(veterinario);
+        }
+
+        private static void AddMascota()
+        {
+            var mascota = new Mascota
+            {
+                
+                Nombre = "Firulay",
+                Color = "Negro",
+                Especie = "Canino",
+                Raza = "Cacri",
+                //DuenoId = "1",
+                //VeterinarioId = "1",
+                //HistoriaId = "1"
+            };
+            _repoMascota.AddMascota(mascota);
         }
     }
 }
