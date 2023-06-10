787f7afac67ee2b1ff51f68f2c706aa49ccf33aa
Josttin Mendez <justinstebanmendez@gmail.com>
2023-06-10 03:38:46 -0500

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -7,45 +7,21 @@ const greeting = `
  |_____/|_|_| |_|\__|    |_|  |_| |_|\___||_|
 `;
 
-const farewell = `
-  _______ _            _____ _             
- |__   __| |          / ____(_)            
-    | |  | |__   ___ | (___  _ _ __   __ _ 
-    | |  | '_ \ / _ \ \___ \| | '_ \ / _\` |
-    | |  | | | |  __/ ____) | | | | | (_| |
-    |_|  |_| |_|\___||_____/|_|_| |_|\__, |
-                                     __/ |
-                                    |___/ 
-`;
-
 function animateASCII(text, speed) {
   const lines = text.split('\n');
   let index = 0;
-  let reverse = false;
 
   setInterval(() => {
     console.clear();
     console.log(lines[index]);
 
-    if (reverse && index === 0) {
-      reverse = false;
-    } else if (!reverse && index === lines.length - 1) {
-      reverse = true;
-    }
-
-    if (reverse) {
-      index--;
-    } else {
-      index++;
+    index++;
+    if (index === lines.length) {
+      index = 0;
     }
   }, speed);
 }
 
-// Saludo
-animateASCII(greeting, 2000);
+animateASCII(greeting, 200);
 
-// Despedida después de 5 segundos
-setTimeout(() => {
-  animateASCII(farewell, 2000);
-}, 5000);
 
